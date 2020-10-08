1e9ae0ca8135b44477b154aba2bba8e593df8fd4
druid <1837039333@qq.com>
2020-10-08 16:40:10 +0800

    add AppRun

---
+++ el-system/src/main/java/com/xubo/AppRun.java
@@ -0,0 +1,21 @@
+package com.xubo;/**
+ * @Author Druid_Xu
+ * @Create 2020/10/8 下午 04:38
+ * TODO :
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author Druid_Xu
+ * @Description TODO
+ * @Date 2020/10/8 下午 04:38
+ */
+@SpringBootApplication
+public class AppRun {
+    public static void main(String[] args) {
+        SpringApplication.run(AppRun.class,args);
+    }
+
+}
