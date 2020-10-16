b48473de2e7f241567ef36f215fbd08e3edc717c
druid <1837039333@qq.com>
2020-10-16 16:52:41 +0800

    update

--- el-system/src/main/java/com/xubo/modules/security/config/ConfigBeanConfiguration.java
+++ el-system/src/main/java/com/xubo/modules/security/config/ConfigBeanConfiguration.java
@@ -13,6 +13,7 @@ import com.xubo.modules.security.config.bean.SecurityProperties;
  * @Description 这个配置类里面可以定义一个或者多个bean,Spring Boot 启动的时候会自动加载配置类和启动类,
  * @Configuration 相当于 用xml 配置Spring的 <beans>  @Bean 相当于 <bean>
  * @ConfigurationProperties 这个注解是读取 application.yml 里面的配置信息，在springboot 启动的时候回给LoginProperties 的 LoginCode赋值
+ *
  */
 @Configuration
 public class ConfigBeanConfiguration {
